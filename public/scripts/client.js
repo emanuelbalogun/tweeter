27696849c390bf24bf1b27288ec035d80b0ddd92
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-07-07 21:09:45 -0500

    Implement tweet Helper function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -38,22 +38,45 @@ const html = `<article>
 return html;
 }
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const renderTweets = function(tweetArray) {
+  tweetArray.forEach((item) => {
+    let $article = createTweetElement (item);
+    $('#tweets-container').append($article); 
+  })
+
+};
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 
 
 const $tweet = createTweetElement(tweetData);
 
+
+renderTweets(data)
 // Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
+//console.log($tweet); // to see what it looks like
 //$('#tweets-container').append($tweet); 
